a0519235fac81e6add349b8ed1fcd346dcc27124
FKzenos <mokraneboukaouma2@gmail.com>
2024-01-31 12:41:37 +0100

    le programme demande si l'utilisateur veux relancé le programme ou le terminer

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
-a = int(input("donne un nombre"))
-print(a)
-b=1
-for i in range(10):
-    c = a * b
-    b=b+1
-    print(c)
+re = True
+while re == True :
+    a = int(input("donne un nombre"))
+    b=1
+    for i in range(10):
+        c = a * b
+        b=b+1
+        print(c)
+    d = input("pour recommencer le programme taper sur 'r' ou taper sur une autre touches pour términer le programme")
+    if d == "r":
+        re = True
+    else :
+        re = False
